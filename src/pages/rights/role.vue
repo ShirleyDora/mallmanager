c9746d52e5cd725f990f854a4167c9b641891493
xueling <xueling.du@definesys.com>
2020-12-21 15:01:43 +0800

    权限管理-角色列表页，展开行渲染表格数据，修改role.vue

--- src/pages/rights/role.vue
+++ src/pages/rights/role.vue
@@ -1,5 +1,5 @@
 <template>
-<el-card class="box-card">
+  <el-card class="box-card">
     <!-- 1.面包屑 -->
     <my-bread level1="权限管理" level2="角色列表"></my-bread>
     <!-- 2.添加按钮 -->
@@ -8,7 +8,56 @@
         <el-button type="info">添加角色</el-button>
       </el-col>
     </el-row>
-</el-card>
+    <!-- 3.表格展示 -->
+    <el-table :data="rolelist" style="width: 100%;" max-height="600">
+      <!-- 展开行 -->
+      <el-table-column type="expand" width="150">
+        <template slot-scope="scope">
+
+        </template>
+      </el-table-column>
+      <!-- 序号 -->
+      <el-table-column type="index" label="#" width="150"></el-table-column>
+      <el-table-column
+        prop="roleName"
+        label="角色名称"
+        width="200"
+      ></el-table-column>
+      <el-table-column
+        prop="roleDesc"
+        label="角色描述"
+        width="200"
+      ></el-table-column>
+      <el-table-column prop="" label="操作">
+        <template slot-scope="scope">
+          <el-button
+            class="edit"
+            size="mini"
+            plain
+            type="primary"
+            icon="el-icon-edit"
+            circle
+          ></el-button>
+          <el-button
+            class="delete"
+            size="mini"
+            plain
+            type="danger"
+            icon="el-icon-delete"
+            circle
+          ></el-button>
+          <el-button
+            class="check"
+            size="mini"
+            plain
+            type="success"
+            icon="el-icon-user"
+            circle
+          ></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
@@ -30,12 +79,11 @@ export default {
   }
 }
 </script>
-
 <style scoped>
 .box-card {
   height: 100%;
 }
-.addRoleBtn{
-  margin-top:20px;
+.addRoleBtn {
+  margin-top: 20px;
 }
 </style>
