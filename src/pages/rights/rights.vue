be1d434942e2d6499b6a8368f5022587c1921f8e
xueling <xueling.du@definesys.com>
2020-12-21 11:58:35 +0800

    自定义面包屑组件，封装引用，修改rights.vue,users.vue,main.js,新增myBread.vue,components.js

--- src/pages/rights/rights.vue
+++ src/pages/rights/rights.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-rights
-  </div>
+  <el-card class="box-card">
+    <!-- 1.面包屑 -->
+    <!-- <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb> -->
+    <my-bread level1="权限管理" level2="权限列表"></my-bread>
+    <!-- 2.表格 -->
+  </el-card>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style scoped>
-
+.box-card {
+  height: 100%;
+}
 </style>
---
+++ src/plugins/components.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+
+import MyBread from '@/components/common/myBread.vue'
+Vue.component('my-bread', MyBread)
